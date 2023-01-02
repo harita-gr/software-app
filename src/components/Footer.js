ae95c4625272c630dd123cd4f38616d7be73d110
Harita Ravindranath <haritagr19@gmail.com>
2023-01-02 23:09:04 +0530

    footer resp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,37 @@ import React from "react";
 const Footer = () => {
   return (
     <Box display="flex" flexDirection="column">
-      <Typography variant="h3" paddingTop={10} textAlign="center">
+      <Typography
+        fontSize={{ lg: 30, md: 24, sm: 18, xs: 14 }}
+        variant="h3"
+        paddingTop={10}
+        textAlign="center"
+      >
         {" "}
         Ready For Any Team Size
       </Typography>
-      <Typography variant="div" padding={4} textAlign="center">
+      <Typography
+        fontSize={{ lg: 24, md: 20, sm: 16, xs: 14 }}
+        variant="div"
+        padding={4}
+        textAlign="center"
+      >
         {" "}
         Optimized For Any Team Size
       </Typography>
       <Box display={"flex"} margin="auto" justifyContent={"center"} padding={5}>
-        <Button sx={{ mr: 2 }} variant="contained">
+        <Button
+          sx={{ mr: 2 }}
+          fontSize={{ lg: 20, md: 17, sm: 12, xs: 8 }}
+          variant="contained"
+        >
           Try CodeEnv for Free
         </Button>
-        <Button sx={{ ml: 2 }} variant="outlined">
+        <Button
+          sx={{ ml: 2 }}
+          fontSize={{ lg: 20, md: 17, sm: 12, xs: 8 }}
+          variant="outlined"
+        >
           Talk to Sales
         </Button>
       </Box>
